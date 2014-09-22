9e931db1043954668eaf753096aa9a2961be56bb
Hiroyuki Sano <sh19910711@gmail.com>
2014-09-22 15:25:40 +0900

    bundle gem

---
+++ lib/social_snippet/registry.rb
@@ -0,0 +1,7 @@
+require "social_snippet/registry/version"
+
+module SocialSnippet
+  module Registry
+    # Your code goes here...
+  end
+end
---
+++ lib/social_snippet/registry/version.rb
@@ -0,0 +1,5 @@
+module SocialSnippet
+  module Registry
+    VERSION = "0.0.1"
+  end
+end
