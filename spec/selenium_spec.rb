2d6ada817dc02a2e8ae4f89130234ce02d8b1451
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-01 13:28:57 +0900

    selenium_spec: add tests

--- spec/selenium_spec.rb
+++ spec/selenium_spec.rb
@@ -11,24 +11,43 @@ describe "Selenium" do
 
       context "search repo" do
 
-        let(:input_element) do
+        let(:input_query) do
           wait = ::Selenium::WebDriver::Wait.new(:timeout => 10)
-          wait.until { driver.find_element(:name => "q", :class => "query").displayed? }
-          driver.find_element :name => "q", :class => "query"
+          wait.until { driver.find_element :name => "q", :css => "input.query" }
         end
 
         before do
-          input_element.send_keys "repo"
-          input_element.submit
+          input_query.send_keys "repo"
+          input_query.submit
         end
 
         it { expect(driver.current_url).to eq "http://localhost:19292/search?q=repo" }
 
-      end
+        context "result panels" do
+          let(:panels) do
+            wait = ::Selenium::WebDriver::Wait.new(:timeout => 10)
+            wait.until { driver.find_elements :css => ".contents-region .panel" }
+          end
+          it { expect(panels.empty?).to be_falsey }
+        end
 
-    end
+        context "click add link" do
 
-  end
+          let(:link_element) do
+            wait = ::Selenium::WebDriver::Wait.new(:timeout => 10)
+            wait.until { driver.find_element :css => 'a[href="/new/repository"]' }
+          end
+
+          before { link_element.click }
+          it { expect(driver.current_url).to eq "http://localhost:19292/new/repository" }
+
+        end # click add link
+
+      end # search repo
+
+    end # open home
+
+  end # :open_home
 
   describe "phantomjs" do
     include_context :phantomjs_1024x768
@@ -40,4 +59,4 @@ describe "Selenium" do
     include_context :open_home
   end
 
-end
+end # Selenium
